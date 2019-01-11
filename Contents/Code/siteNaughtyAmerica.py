edf60edafce7aeb8fb9cf7b066a616030934311f
jonathangraber-bolis <jonathangraber-bolis@192.168.0.228>
2019-01-11 21:06:05 +0100

    Bug Fix NaughtyAmerica

--- Contents/Code/siteNaughtyAmerica.py
+++ Contents/Code/siteNaughtyAmerica.py
@@ -3,7 +3,7 @@ import PAgenres
 def search(results,encodedTitle,title,searchTitle,siteNum,lang,searchByDateActor,searchDate,searchAll,searchSiteID):
     searchString = encodedTitle.replace(" ","+")
     if not searchAll:
-        searchString = searchString + "+in+" + PAsearchSites.getSearchSiteName(searchSiteID).replace(" ","+")
+        searchString = searchString + "+" + PAsearchSites.getSearchSiteName(searchSiteID).replace(" ","+")
     searchResults = HTML.ElementFromURL(PAsearchSites.getSearchSearchURL(siteNum) + searchString)
     for searchResult in searchResults.xpath('//div[@class="scene-item"]'):
         titleNoFormatting = searchResult.xpath('.//a')[0].get("title")
@@ -36,7 +36,7 @@ def update(metadata,siteID,movieGenres,movieActors):
     #paragraph = detailsPageElements.xpath('//span[@class="moreless js-readmore"]')[0].text_content()
     #paragraph = paragraph.replace('&13;', '').strip(' \t\n\r"').replace('\n','').replace('  ','') + "\n\n"
     metadata.summary = detailsPageElements.xpath('//p[@class="synopsis_txt"]')[0].text_content().strip()
-    subSite = detailsPageElements.xpath('//a[@class="site-title grey-text"]')[0].text_content()
+    subSite = detailsPageElements.xpath(("//div[@class='scene-info']/a[@class='site-title grey-text link']"))[0].text_content()
     metadata.tagline = subSite
     metadata.collections.clear()
     metadata.collections.add(metadata.tagline)
@@ -49,13 +49,16 @@ def update(metadata,siteID,movieGenres,movieActors):
         
     # Actors
     movieActors.clearActors()
-    actors = detailsPageElements.xpath('//a[@class="scene-title grey-text"]')
+    actors = detailsPageElements.xpath('//a[contains(@class,"scene-title grey-text")]')
     if len(actors) > 0:
         for actorLink in actors:
             actorName = actorLink.text_content()
             actorPageURL = actorLink.get("href")
             actorPage = HTML.ElementFromURL(actorPageURL)
-            actorPhotoURL = "http:" + actorPage.xpath('//img[@class="performer-pic"]')[0].get("src")
+            try:
+                actorPhotoURL = "http:" + actorPage.xpath('//img[@class="performer-pic"]')[0].get("src")
+            except:
+                actorPhotoURL = ""
             movieActors.addActor(actorName,actorPhotoURL)
 
     # Genres
